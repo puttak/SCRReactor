951dc817eff7e969315528402e007a5fd2fce1db
Luke Fulford <luke@luke.scot>
2017-02-13 01:14:59 +0000

    Add some comments and cleanup data

--- initialFlueGasFlow.m
+++ initialFlueGasFlow.m
@@ -1,3 +1,4 @@
+% This function stores and returns the molar flows in kmol/s 
 function [data] = initialFlueGasFlow()
 
 data.molarFlows.oxygen = 2.02e-1;
@@ -10,30 +11,4 @@ data.molarFlows.sulphurDioxide = 1.68e-5;
 data.molarFlows.sulphurTrioxide = 6.84e-6;
 data.molarFlows.nitrogenOxide = 1.34e-3;
 data.molarFlows.nitrogenDioxide = 2.39e-6;
-
-data.totalMolarFlow = sum(cell2mat(struct2cell(data.molarFlows))); % This isn't pretty but does the job
-
-data.moleFraction.oxygen = data.molarFlows.oxygen/data.totalMolarFlow;
-data.moleFraction.carbonDioxide = data.molarFlows.carbonDioxide/data.totalMolarFlow;
-data.moleFraction.water = data.molarFlows.water/data.totalMolarFlow;
-data.moleFraction.ammonia = data.molarFlows.ammonia/data.totalMolarFlow;
-data.moleFraction.nitrogen = data.molarFlows.nitrogen/data.totalMolarFlow;
-data.moleFraction.hydrogenChloride = data.molarFlows.hydrogenChloride/data.totalMolarFlow;
-data.moleFraction.sulphurDioxide = data.molarFlows.sulphurDioxide/data.totalMolarFlow;
-data.moleFraction.sulphurTrioxide = data.molarFlows.sulphurTrioxide/data.totalMolarFlow;
-data.moleFraction.nitrogenOxide = data.molarFlows.nitrogenOxide/data.totalMolarFlow;
-data.moleFraction.nitrogenDioxide = data.molarFlows.nitrogenDioxide/data.totalMolarFlow;
-
-data.molarWeight.oxygen = 32;
-data.molarWeight.carbonDioxide = 44;
-data.molarWeight.water = 18;
-data.molarWeight.ammonia = 17;
-data.molarWeight.nitrogen = 28;
-data.molarWeight.hydrogenChloride = 36.5;
-data.molarWeight.sulphurDioxide = 64;
-data.molarWeight.sulphurTrioxide = 80;
-data.molarWeight.nitrogenOxide = 30;
-data.molarWeight.nitrogenDioxide = 46;
-
-data.molarWeight.average = sum(cell2mat(struct2cell(data.molarWeight)).*cell2mat(struct2cell(data.moleFraction)));
 end
--- shomateLoader.m
+++ shomateLoader.m
@@ -1,21 +1,27 @@
-function [WB, Hf298] = shomateLoader(desiredComponents)
+% This function loads the data nessisary to use the shomate equation. The
+% data is loaded from a JSON file, using the JSONLAB toolbox; and then
+% manipulated into the format needed.
+
+function [shomate, Hf298] = shomateLoader(desiredComponents)
 gibbsData = loadjson('shomate.json');
 for i=1:length(desiredComponents)
     j = 1;
     found = 0;
     while j<=length(gibbsData) && found == 0
         if strcmp(gibbsData{j}.name, desiredComponents{i})
-            %fprintf('FOUND %s\n',gibbsData{j}.name);
             found = 1;
         else
+            % Record wasn't found so increase the count and repeat
             j=j+1;
         end
     end
     if found == 0
+        % Data for the desired component was not found, so print an error
+        % message.
         fprintf('COULD NOT FIND %s\n',desiredComponents{i});
     else
         % We found the record
-        WB(i,:) = gibbsData{j}.shomate;
+        shomate(i,:) = gibbsData{j}.shomate;
         Hf298(i,:) = gibbsData{j}.H_f298;
     end
 end
